d9b4379ba71f2bdf243b7b298b21b448f887272e
NikolaGrdanoski <ngrdanoski@gmail.com>
2025-04-13 20:11:26 +0200

    Initial Commit

---
+++ OurDoorApp/OurDoorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OurDoorAppApp.swift
+//  OurDoorApp
+//
+//  Created by Nikola Grdanoski on 13.4.25.
+//
+
+import SwiftUI
+
+@main
+struct OurDoorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
