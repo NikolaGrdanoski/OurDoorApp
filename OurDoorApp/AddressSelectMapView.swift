a5e46455b26b74ba29963e571bb313b7247aaeee
NikolaGrdanoski <ngrdanoski@gmail.com>
2025-04-19 15:52:45 +0200

    Added a map

---
+++ OurDoorApp/AddressSelectMapView.swift
@@ -0,0 +1,18 @@
+//
+//  AddressSelectMapView.swift
+//  OurDoorApp
+//
+//  Created by Nikola Grdanoski on 19.4.25.
+//
+
+import SwiftUI
+
+struct AddressSelectMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressSelectMapView()
+}
